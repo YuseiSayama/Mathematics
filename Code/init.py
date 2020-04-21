51eff519f0477fd07acace6eb0a404b26a8f29bd
YuseiSayama <62337572+YuseiSayama@users.noreply.github.com>
2020-04-21 19:06:06 +0900

    初期設定

---
+++ Code/init.py
@@ -0,0 +1,7 @@
+import os
+
+os.makedirs('../BackNumber', exist_ok=True)
+os.makedirs('../Edit/Splited', exist_ok=True)
+os.makedirs('../Image', exist_ok=True)
+os.makedirs('../Renew', exist_ok=True)
+os.makedirs('../Storage', exist_ok=True)
